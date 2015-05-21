dc7dc451133260faf84f6d505b33728c55adcdc9
dchambers <dominic.chambers@gmail.com>
2015-05-21 09:17:03 +0100

    Updated spike to include an example of a forked version dependency.

--- JSPMLib/config.js
+++ JSPMLib/config.js
@@ -39,17 +39,43 @@ System.config({
     "babel": "npm:babel-core@5.4.3",
     "babel-runtime": "npm:babel-runtime@5.4.3",
     "core-js": "npm:core-js@0.9.11",
+    "github:jspm/nodelibs-domain@0.1.0": {
+      "domain-browser": "npm:domain-browser@1.1.4"
+    },
+    "github:jspm/nodelibs-events@0.1.0": {
+      "events-browserify": "npm:events-browserify@0.0.1"
+    },
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
     },
     "npm:CJSDependentLib@0.0.0": {
       "CJSLib": "npm:CJSLib@0.0.0",
-      "ES6Lib": "npm:ES6Lib@0.0.0"
+      "ES6Lib": "npm:ES6Lib@0.0.0",
+      "asap": "npm:asap@2.0.3"
+    },
+    "npm:CJSLib@0.0.0": {
+      "asap": "npm:asap@1.0.0"
+    },
+    "npm:ES6Lib@0.0.0": {
+      "asap": "npm:asap@2.0.3"
+    },
+    "npm:asap@1.0.0": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:asap@2.0.3": {
+      "domain": "github:jspm/nodelibs-domain@0.1.0",
+      "process": "github:jspm/nodelibs-process@0.1.1"
     },
     "npm:core-js@0.9.11": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "process": "github:jspm/nodelibs-process@0.1.1",
       "systemjs-json": "github:systemjs/plugin-json@0.1.0"
+    },
+    "npm:domain-browser@1.1.4": {
+      "events": "github:jspm/nodelibs-events@0.1.0"
+    },
+    "npm:events-browserify@0.0.1": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
     }
   }
 });
